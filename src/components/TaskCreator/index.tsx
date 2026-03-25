00883e182b7d60962693855e184f454069ef1b17
Bernard Krehula <krehula.bernard456@gmail.com>
2026-03-25 18:57:00 +0100

    feat(api): upload new task to realtime storage on supabase in <TaskCreator> component

--- src/components/TaskCreator/index.tsx
+++ src/components/TaskCreator/index.tsx
@@ -1,22 +1,43 @@
-import './index.css';
-import Input from '../../ui/input';
-import Btn from '../../ui/btn';
-import { useState } from 'react';
+import "./index.css";
+import Input from "../../ui/input";
+import Btn from "../../ui/btn";
+import { useState } from "react";
+import { getDatabase, push, ref, set } from "firebase/database";
+import app from "../../config";
 
 const TaskCreator = () => {
-    const [inputValue, setInputValue] = useState<string>("");
+  const [inputValue, setInputValue] = useState<string>("");
 
-    const changeInptuValue = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const value = e.target.value;
-        setInputValue(value);
+  const changeInptuValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setInputValue(value);
+  };
+  const saveTask = () => {
+    try {
+      const db = getDatabase(app);
+      const newDocRef = push(ref(db, "tasks"));
+      set(newDocRef, {
+        taskName: inputValue,
+      });
+      clearInput();
+      return console.log("data saved succesfully");
+    } catch (error) {
+      throw Error;
     }
-    const clearInput = () => setInputValue("");
+  };
+  const clearInput = () => setInputValue("");
 
-    return(
-        <div className='task-creator'>
-            <Input placeholder="Add your task" onChange={changeInptuValue} value={inputValue}/>
-            <Btn type='button' variation="primary" onClick={clearInput}>+</Btn>
-        </div>
-    )
-}
+  return (
+    <div className="task-creator">
+      <Input
+        placeholder="Add your task"
+        onChange={changeInptuValue}
+        value={inputValue}
+      />
+      <Btn type="button" variation="primary" onClick={saveTask}>
+        +
+      </Btn>
+    </div>
+  );
+};
 export default TaskCreator;
